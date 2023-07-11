f2f20d44547c17a9e7d4258d5429baa2b24755c8
Jesus Pastor <jnavalon@biit-solutions.com>
2023-07-11 16:18:38 +0200

    add scripts to copy translations from biit-ui

--- transloco.config.js
+++ transloco.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   rootTranslationsPath: 'src/assets/i18n/',
   langs: ['en', 'es', 'nl'],
-  keysManager: {},
-  scopedLibs: [
-    {
-      src: './projects/biit-ui/lib1',
-      dist: ['./src/assets/i18n/biit-ui']
-    }
-  ]
-
+  keysManager: {}
 };
