8f383891af2bc5ed5bb825fefb8f689907943103
Jesus Pastor <jnavalon@biit-solutions.com>
2023-10-26 13:57:46 +0200

    removed constants

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,9 +1,4 @@
 export class Constants {
-   public static readonly SESSION_STORAGE = class {
-     public static readonly AUTH_TOKEN: string = 'authToken';
-     public static readonly AUTH_EXPIRATION: string = 'authExp';
-     public static readonly USER: string = 'user';
-   }
 
   public static readonly PATHS = class {
      public static readonly PORTAL: string = '/users';
