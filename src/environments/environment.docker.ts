bfe721e5f4a894a30bd9ac9d9fdb57ed568026d7
Jorge Hortelano <jhortelano@biit-solutions.com>
2023-11-03 12:46:44 +0100

    Prepared docker variables

---
+++ src/environments/environment.docker.ts
@@ -0,0 +1,4 @@
+export class Environment {
+  public static readonly ROOT_URL: string = 'PROTOCOL://MACHINE_DOMAIN';
+  public static readonly USER_MANAGER_PATH: string = 'user-manager-system-backend';
+}
