68114f4481bb931c06c994d7a4aa07858a9e20e3
kbenlloch <kbenlloch@biit-solutions.com>
2024-02-28 15:37:08 +0100

    #221 Added Cookies Consent component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {BiitSnackbarModule} from "biit-ui/info";
+import {BiitCookiesConsentModule, BiitSnackbarModule} from "biit-ui/info";
 import {TranslocoRootModule} from "biit-ui/i18n";
 import {CommonModule, registerLocaleData} from "@angular/common";
 import {HeaderInterceptor} from "./config/header-interceptor";
@@ -23,17 +23,18 @@ registerLocaleData(localeNL, 'nl');
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    CommonModule,
-    AppRoutingModule,
-    HttpClientModule,
-    TranslocoRootModule,
-    BiitSnackbarModule,
-    BiitNavUserModule,
-    BiitNavMenuModule,
-    BiitNavbarModule
-  ],
+    imports: [
+        BrowserModule,
+        CommonModule,
+        AppRoutingModule,
+        HttpClientModule,
+        TranslocoRootModule,
+        BiitSnackbarModule,
+        BiitNavUserModule,
+        BiitNavMenuModule,
+        BiitNavbarModule,
+        BiitCookiesConsentModule
+    ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: HeaderInterceptor, multi: true}
   ],
