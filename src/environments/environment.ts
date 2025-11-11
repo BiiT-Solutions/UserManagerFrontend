a11569a00f98308508387b9e04f94dbca06b99a3
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-11-11 15:10:31 +0100

    Releasing as open source.

---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export class Environment {
+  public static readonly ROOT_URL: string = 'https://testing.server.com';
+  public static readonly USER_MANAGER_PATH: string = '/user-manager-system-backend';
+}
